a19bd63de9c00f251538b0e0c704e6b869395f54
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-04-05 21:54:45 -0400

    add preliminary pydantic support

--- bigbrother/tests/common.py
+++ bigbrother/tests/common.py
@@ -6,6 +6,8 @@ def create_tester(instance, method_name, expected_args, expected_kwargs):
 
     def track_changes(obj, method, *args, **kwargs):
         called.append(True)
+        print(f"method: {method} args: {args} kwargs: {kwargs}")
+        assert obj == instance
         assert method == method_name
         assert args == expected_args
         assert kwargs == expected_kwargs
